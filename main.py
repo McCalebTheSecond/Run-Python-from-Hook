d4601e2b64bcaa6a1f5c7b49f82a09540c53bb04
McCalebTheFirst <127219201+McCalebTheSecond@users.noreply.github.com>
2023-03-22 14:02:03 +0000

    Receives Python and returns data in results variable. Example: a = 6 b = 7 c = a + b result = { 'data': c } Result sent out in JOSN format:  "data": {             "data": 13         }

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ app = Flask(__name__)
 
 @app.route('/execute', methods=['POST'])
 def execute_code():
-    code = request.json.get('code')
-    if code is None:
+    code = request.data.decode('utf-8')
+    if code is None or code == "":
         return jsonify({'error': 'No code provided'}), 400
 
     try:
         exec_globals = {}
         exec(code, exec_globals)
-        results = {k: v for k, v in exec_globals.items() if not k.startswith('_')}
-        return jsonify({'results': results})
+        result = exec_globals.get('result')
+        if result is None:
+            return jsonify({'error': 'No result variable found'}), 400
+        return jsonify(result)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-    
