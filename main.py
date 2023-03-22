d08d163c453d46c571b5ad0b7946ac25c78608f3
McCalebTheFirst <127219201+McCalebTheSecond@users.noreply.github.com>
2023-03-22 14:31:13 +0000

    Console shows what's going on.

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ app = Flask(__name__)
 @app.route('/execute', methods=['POST'])
 def execute_code():
     code = request.data.decode('utf-8')
+    print(f"Received code:\n{code}")
+    
     if code is None or code == "":
+        print("Error: No code provided")
         return jsonify({'error': 'No code provided'}), 400
 
     try:
         exec_globals = {}
         exec(code, exec_globals)
         result = exec_globals.get('result')
+
         if result is None:
+            print("Error: No result variable found")
             return jsonify({'error': 'No result variable found'}), 400
+
+        print(f"Result: {result}")
         return jsonify(result)
     except Exception as e:
+        print(f"Error: {str(e)}")
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
